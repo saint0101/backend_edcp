67236540b60f32e7e0eeeb4cc3a007d7d9d8333a
SAINT FOURIER ONESYME <saintfourieronesyme@gmail.com>
2024-05-15 12:12:44 +0000

    creation de la commande d'attente du demarrage de la BD

--- app/edcp_apirest/management/commands/wait_for_db.py
+++ app/edcp_apirest/management/commands/wait_for_db.py
@@ -16,18 +16,18 @@ class Command(BaseCommand):
         """
         Point d'entrée de la commande.
         """
-        # Afficher un message pour indiquer que nous attendons que la base de données soit disponible
+        # Afficher un message pour indiquer que nous
+        # attendons que la base de données soit disponible
         self.stdout.write('En attente de la base de données...')
-
         # Variable pour suivre l'état de la base de données
         db_up = False
-
         # Boucle pour attendre que la base de données soit disponible
         while db_up is False:
             try:
                 # Vérifier l'état de la base de données
                 self.check(databases=['default'])
-                db_up = True  # Marquer la base de données comme disponible si la vérification réussit
+                # Marquer la base de données comme disponible si la vérification réussit
+                db_up = True
             except (Psycopg2OpError, OperationalError):
                 # La base de données n'est pas disponible, attendre 1 seconde avant de réessayer
                 self.stdout.write('Base de données non disponible, attente de 1 seconde...')
--- app/edcp_apirest/tests/test_commands.py
+++ app/edcp_apirest/tests/test_commands.py
@@ -10,24 +10,37 @@ from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
+# Substitution de la méthode 'check'
+# dans la classe 'Command' du module 'wait_for_db'
 @patch('edcp_apirest.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-    """Test de la commande"""
+    """Tests de la commande"""
 
+    # Test de vérification si la base de données est prête
     def test_wait_for_db_ready(self, patched_check):
-        """Test d'attente de la base de données si la bd est disponible"""
+        """Test d'attente de la base de données si elle est disponible"""
+        # Définition de la valeur de retour de la méthode 'check' simulée à True
         patched_check.return_value = True
 
+        # Appel de la commande 'wait_for_db'
         call_command('wait_for_db')
 
+        # Vérification que la méthode 'check' a été appelée une fois avec databases=['default']
         patched_check.assert_called_once_with(databases=['default'])
 
+    # Test d'attente de la base de données avec des erreurs opérationnelles
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-        """Test waiting for database when getting OperationalError."""
+        """Test d'attente de la base de données en cas d'erreur opérationnelle."""
+        # Configuration de l'effet secondaire de la méthode 'check'
+        # simulée pour lever Psycopg2Error deux fois,
+        # OperationalError trois fois, puis retourner True
         patched_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
 
+        # Appel de la commande 'wait_for_db'
         call_command('wait_for_db')
 
+        # Vérification que la méthode 'check' a été appelée six fois
         self.assertEqual(patched_check.call_count, 6)
+        # Vérification que la méthode 'check' a été appelée avec databases=['default']
         patched_check.assert_called_with(databases=['default'])
